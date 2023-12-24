4b2449818882b75fd1af2cf12a86f3dad2e874f5
mehedi hasan <mdchanchal5675@gmail.com>
2023-12-25 00:07:16 +0600

    firebase .env.local file add

--- src/_firebase/firebase.config.js
+++ src/_firebase/firebase.config.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCo0jEdljro4Fuoox3hvwIIww867l1-mY0",
-  authDomain: "diuswe-cpsheet.firebaseapp.com",
-  projectId: "diuswe-cpsheet",
-  storageBucket: "diuswe-cpsheet.appspot.com",
-  messagingSenderId: "249004902554",
-  appId: "1:249004902554:web:a15ec2ad60a6e3a2c27723",
-  measurementId: "G-E936GMMLKT"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
+  measurementId:import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
